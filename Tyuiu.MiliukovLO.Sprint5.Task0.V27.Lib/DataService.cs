4e3d91102f33f1451d6694525d42fba28cf4aa58
ssadown <bibatututu@yandex.ru>
2024-10-21 19:45:35 +0500

    Add project files.

---
+++ Tyuiu.MiliukovLO.Sprint5.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MiliukovLO.Sprint5.Task0.V27.Lib
+{
+    public class DataService : ISprint5Task0V27
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double result = Math.Pow(x - 1, 3 * x + 1);
+
+            double roundedResult = Math.Round(result, 3);
+
+            string tempFilePath = Path.GetTempFileName();
+
+            File.WriteAllText(tempFilePath, roundedResult.ToString());
+
+            return tempFilePath;
+        }
+    }
+}
---
+++ Tyuiu.MiliukovLO.Sprint5.Task0.V27/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.MiliukovLO.Sprint5.Task0.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new();
+        Console.Title = "Спринт #5 | Выполнил: Милюков Л. О. | ИИПб 24-2";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                                ");
+        Console.WriteLine("* Тема: Запись данных в текстовый файл                                     ");
+        Console.WriteLine("* Задание #0                                                               ");
+        Console.WriteLine("* Вариант #27                                                              ");
+        Console.WriteLine("* Выполнил: Милюков Леонид Олегович | ИИПб 24-2                            ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в   ");
+        Console.WriteLine("* екстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх");
+        Console.WriteLine("* знаков после запятой.                                                    ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+        int x = 3;
+        Console.WriteLine("*                                                                          ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+        Console.WriteLine("***************************************************************************");
+        string result = ds.SaveToFileTextData(x);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
