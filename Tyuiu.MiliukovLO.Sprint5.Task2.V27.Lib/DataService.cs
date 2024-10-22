e3bd2408e31d323e4650679d02c8e938e285269d
ssadown <bibatututu@yandex.ru>
2024-10-22 20:00:39 +0500

    Task2

--- Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
@@ -1,4 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
 {
@@ -7,7 +9,7 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
         public string SaveToFileTextData(int[,] matrix)
         {
             string tempFilePath = Path.GetTempFileName();
-            string finalPath = Path.Combine(tempFilePath, "OutPutFileTask2.csv");
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -16,11 +18,16 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
                     {
                         matrix[i, j] = 0;
                     }
-                    File.AppendAllText(finalPath, $"{matrix[i, j]}");
+                    File.AppendAllText(tempFilePath, $"{matrix[i, j]}");
+                    if (j < matrix.GetLength(1) - 1)
+                    {
+                        File.AppendAllText(tempFilePath, ";");
+                    }
                 }
-                File.AppendAllText(finalPath, $"\n");
+                File.AppendAllText(tempFilePath, "n");
             }
-            return finalPath;
+
+            return tempFilePath;
         }
     }
 }
