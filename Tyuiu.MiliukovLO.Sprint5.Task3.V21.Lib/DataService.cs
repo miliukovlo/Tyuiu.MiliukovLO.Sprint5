2928d007871284ddc52679d2bcd2e5f83de5055e
ssadown <bibatututu@yandex.ru>
2024-10-22 21:05:04 +0500

    Task3

--- Tyuiu.MiliukovLO.Sprint5.Task3.V21.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint5.Task3.V21.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task3.V21.Lib
         public string SaveToFileTextData(int x)
         {
             string tempFile = Path.GetTempFileName();
-            double res = (Math.Pow(x,2)+1)/Math.Sqrt(4 * Math.Pow(x,2) - 3);
+            double res = Math.Round((Math.Pow(x,2)+1)/Math.Sqrt(4 * Math.Pow(x,2) - 3),3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(tempFile, FileMode.Append)))
             {
                 writer.Write(res);
