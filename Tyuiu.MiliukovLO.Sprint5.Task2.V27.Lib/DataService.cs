74de9dd15bbda5bae1e9acd8aa05dfe5e9ac938b
ssadown <bibatututu@yandex.ru>
2024-10-22 20:10:01 +0500

    Task2

--- Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
@@ -10,21 +10,22 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
         {
             string tempFilePath = Path.GetTempFileName();
 
-            for (int i = 0; i <= matrix.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j <= matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if ((i + j) % 2 != 0)
                     {
                         matrix[i, j] = 0;
                     }
-                    File.AppendAllText(tempFilePath, $"{matrix[i, j]}");
-                    if (j <= matrix.GetLength(1) - 1)
+                    //File.AppendAllText(tempFilePath, $"{matrix[i, j]}");
+                    if (j < matrix.GetLength(1) - 1)
                     {
-                        File.AppendAllText(tempFilePath, ";");
+                        //File.AppendAllText(tempFilePath, ";");
                     }
                 }
-                File.AppendAllText(tempFilePath, "\n");
+                //File.AppendAllText(tempFilePath, "\n");
+                File.AppendAllText(tempFilePath, "");
             }
 
             return tempFilePath;
