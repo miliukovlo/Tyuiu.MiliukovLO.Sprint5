28bddc22f7e33a661cb3460b27b5407c7be2c5ab
ssadown <bibatututu@yandex.ru>
2024-10-22 19:41:45 +0500

    Task2

--- Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib/DataService.cs
@@ -8,6 +8,7 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
         {
             string tempFilePath = Path.GetTempFileName();
             string finalPath = Path.Combine(tempFilePath, "OutPutFileTask2.csv");
+            int[] tempMatrix = [0, 0, 0];
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -16,7 +17,9 @@ namespace Tyuiu.MiliukovLO.Sprint5.Task2.V27.Lib
                     {
                         matrix[i, j] = 0;
                     }
+                    tempMatrix[j] = matrix[i, j];
                 }
+                File.WriteAllText(finalPath, tempMatrix.ToString());
             }
             return tempFilePath;
         }
